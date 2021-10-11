3b4e3923161d81f8c133949d2133a10609a21c5f
SeaWar741 <juanca741@gmail.com>
2021-10-11 09:58:44 -0500

    added security bonus

--- app.js
+++ app.js
@@ -5,12 +5,23 @@ const express = require("express");
 const usersRoutes = require("./routes/users");
 const reportsRoutes = require("./routes/reports");
 const categoriesRoutes = require("./routes/categories");
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const limiter = require("./helpers/ratelimiter");
 
 const app = express();
 
+
+//security
 app.use(express.json({ limit: "50mb" }));
 
+app.use(xss());
 
+
+app.use(mongoSanitize());
+
+
+//functionality
 app.use("/users", usersRoutes);
 
 app.use("/reports",reportsRoutes);
